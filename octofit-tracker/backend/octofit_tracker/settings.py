0df9c283c20558481eeaeb8f44a49b2cb30d3edf
Devansh Mistry <73028874+DevanshMistry890@users.noreply.github.com>
2025-04-07 23:20:59 +0000

    step3.1

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.dummy",  # Dummy engine since we are using pymongo
+        "ENGINE": "djongo",
         "NAME": "octofit_db",
         "HOST": "localhost",
         "PORT": 27017,
